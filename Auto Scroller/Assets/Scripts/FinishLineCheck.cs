ae5dcf62a3194629943242b999b2991024a8c3fe
pockerinos <kchen9988@me.com>
2020-09-27 20:03:18 -0400

    Player now stops upon hitting flag
    Player now stops moving when they touch the flag, need to work on stopping the animation.

---
+++ Auto Scroller/Assets/Scripts/FinishLineCheck.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FinishLineCheck : MonoBehaviour
+{
+    public Transform flagCheck;
+    public float checkRadius;
+    public LayerMask flag;
+
+    private bool isFinished;
+    private Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        isFinished = Physics2D.OverlapCircle(flagCheck.position, checkRadius, flag);
+        if (isFinished)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+}
