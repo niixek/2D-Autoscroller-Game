8dc55de1dfeb0a226d7476b205b2da418d5daed5
pockerinos <kchen9988@me.com>
2020-10-26 18:42:34 -0400

    Added level 2 to main menu
    Level 2 can now be accessed from the main menu, can go to main menu from level 2 as well.

--- Auto Scroller/Assets/Scripts/MainMenu.cs
+++ Auto Scroller/Assets/Scripts/MainMenu.cs
@@ -3,9 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGame(int i)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + i);
     }
 
     public void QuitGame()
