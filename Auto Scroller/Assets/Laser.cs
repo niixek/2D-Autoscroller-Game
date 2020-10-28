a9c56fe55a7b49cc856de86121b178a92d977ba6
pockerinos <kchen9988@me.com>
2020-10-28 11:55:26 -0400

    Fixed collision with breakables and added laser

---
+++ Auto Scroller/Assets/Laser.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float laserSpeed = 15f;
+    public float laserBaseDamage = 10f;
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * laserSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
