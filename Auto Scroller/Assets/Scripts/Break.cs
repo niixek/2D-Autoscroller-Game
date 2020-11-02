81dabf023b3f7dd5d6e30f0485173bdd3777c44a
pockerinos <kchen9988@me.com>
2020-11-02 12:29:26 -0500

    Health bar now only appears after taking damage

--- Auto Scroller/Assets/Scripts/Break.cs
+++ Auto Scroller/Assets/Scripts/Break.cs
@@ -1,23 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Break : MonoBehaviour
 {
     public int health;
     public HealthBar healthBar;
     public int maxHealth;
+    public GameObject canvas;
+
+    private bool showing = false;
 
     private void Start()
     {
         healthBar.SetMaxHealth(maxHealth);
+        canvas.SetActive(false);
     }
 
     void Update()
     {
+        if (!showing && (health < maxHealth))
+        {
+            canvas.SetActive(true);
+            showing = true;
+        }
         if (health <= 0)
         {
             Destroy(gameObject);
+            Destroy(canvas);
         }
         healthBar.SetHealth(health);
     }
